36dd150275c0c39312b76d8957dbb1deaff669e4
Ivan Slastikhin <stopgo89@gmail.com>
2020-06-25 20:15:26 +0300

    Изменен конструктор в классе TCPConnection с учетом интерфейса TCPConnectionListener. Добавлены методы для вывода сообщения и закрытия соединения. Переопределен метод toString для логирования соединений. Добавлен новый конструктор с возможностью указания адреса и порта соединения.

--- network/src/ru/jhonsy/network/TCPConnection.java
+++ network/src/ru/jhonsy/network/TCPConnection.java
@@ -13,19 +13,56 @@ public class TCPConnection {
     private final Thread rxThread;
     private final BufferedReader in;
     private final BufferedWriter out;
+    private final TCPConnectionListener eventListener;
 
-    public TCPConnection(Socket socket) throws IOException {
+    public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
+        this.eventListener = eventListener;
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
         this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
-        this.rxThread = new Thread(() -> {
-            try {
-                String msg = in.readLine();
-            } catch (IOException e) {
-
-            } finally {
-
+        this.rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while (!rxThread.isInterrupted()) {
+                        eventListener.onRecieveString(TCPConnection.this, in.readLine());
+                    }
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                } finally {
+                    eventListener.onDisconnect(TCPConnection.this);
+                }
             }
         });
+        rxThread.start();
+    }
+
+    public TCPConnection(TCPConnectionListener eventListener, String ipAddress, int port) throws IOException {
+        this(eventListener, new Socket(ipAddress, port));
+    }
+
+    public synchronized void sendString(String value){
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect();
+        }
+    }
+
+    public synchronized void disconnect(){
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
     }
 }
