5c37ac697d81aca840fe3cf18938f9f30f8948fe
Ivan Slastikhin <stopgo89@gmail.com>
2020-06-25 17:48:57 +0300

    Создан конструктор, принимающий в качестве аргумента Socket.

--- network/src/ru/jhonsy/network/TCPConnection.java
+++ network/src/ru/jhonsy/network/TCPConnection.java
@@ -13,13 +13,11 @@ public class TCPConnection {
     private final Thread rxThread;
     private final BufferedReader in;
     private final BufferedWriter out;
-    private final TCPConnectionListener eventListener;
 
-    public TCPConnection(Socket socket, TCPConnectionListener eventListener) throws IOException {
+    public TCPConnection(Socket socket) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
         this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
-        this.eventListener = eventListener;
         this.rxThread = new Thread(() -> {
             try {
                 String msg = in.readLine();
