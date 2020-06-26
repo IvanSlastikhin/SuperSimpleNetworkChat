fedb82c81a8df9c70f2fb39bffe2db2cf31ef42f
Ivan Slastikhin <stopgo89@gmail.com>
2020-06-26 14:18:44 +0300

    Релизация окна клиента.

---
+++ client/src/ru/jhonsy/chat/client/ClientWindow.java
@@ -0,0 +1,97 @@
+package ru.jhonsy.chat.client;
+
+import ru.jhonsy.network.TCPConnection;
+import ru.jhonsy.network.TCPConnectionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+/**
+ * @Author: Ivan Slastikhin
+ */
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+
+    private static final String IP_ADDRESS = "localhost";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("Anonymous");
+    private final JTextField fieldInput = new JTextField();
+
+    private TCPConnection connection;
+
+    private ClientWindow(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+
+        add(fieldNickname, BorderLayout.NORTH);
+
+        setVisible(true);
+        try {
+            this.connection = new TCPConnection(this, IP_ADDRESS, PORT);
+        } catch (IOException e) {
+            printMessage("Connection exception: " + e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String message = fieldInput.getText();
+        if (message.equals("")) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + message);
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMessage("Connection ready...");
+    }
+
+    @Override
+    public void onRecieveString(TCPConnection tcpConnection, String value) {
+        printMessage(value);
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMessage("Connection close: " + tcpConnection);
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMessage("Connection exception: " + e);
+    }
+
+    private synchronized void printMessage(String message){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(message + "\r\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
+}
